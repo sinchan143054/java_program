3b6e105802d11149b9648c166e154b9794e7703d
sinchan143054 <sinchan.143054@gmail.com>
2025-04-11 05:34:11 +0530

    save

---
+++ datatype.java
@@ -0,0 +1,18 @@
+public class datatype{
+public static void main(String[] args)
+{
+    char s="D";
+    int i=10;
+    byte n=4;
+    double c=8;
+    long h= 1;
+    short u=3;
+
+    System.out.println("char ="+s);
+    System.out.println("int ="+i);
+    System.out.println("Byte ="+n);
+    System.out.println("Double ="+c);
+    System.out.println("long ="+h);
+    System.out.println("short ="+u);
+}
+}
