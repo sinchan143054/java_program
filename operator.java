8c7d9f31f9280a8cbd5ff10730bddf3012a61099
sinchan143054 <sinchan.143054@gmail.com>
2025-04-11 05:34:58 +0530

    save

---
+++ operator.java
@@ -0,0 +1,12 @@
+public class operator{
+    public static void main(String[] args) {
+        float a=10;
+        float b = 20;
+
+        System.out.println("Addition ="+(a+b));
+        System.out.println("Subtraction ="+(a-b));
+        System.out.println("Multipl ="(a*b));
+        System.out.println("Division = "(a/b));
+        System.out.println("mod ="+(a%b));
+    }
+}
