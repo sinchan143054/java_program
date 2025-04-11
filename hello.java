e0fa43b3a3faa6322b3922cc1a2f17876b758106
sinchan143054 <sinchan.143054@gmail.com>
2025-04-11 05:33:02 +0530

    save

---
+++ hello.java
@@ -0,0 +1,7 @@
+package java_program;
+
+public class hello { public static void main(String[] args) {
+    System.out.println("hello sinchan") ; 
+    System.out.println("hello !!!!!!!!!!!!!!!!!") ; 
+  }
+ }
